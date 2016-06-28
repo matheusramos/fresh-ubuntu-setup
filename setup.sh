7acbf38127f8f1dbc4911ee8601bfa291d24cdc1
Matheus Ramos <matheusedsonramos@gmail.com>
2016-06-27 22:32:42 -0300

    Added bashrc
    Also minor fixes in setup

--- setup.sh
+++ setup.sh
@@ -29,34 +29,34 @@ sudo apt-get update
 sudo apt-get install -y build-essential exuberant-ctags dh-make debhelper fakeroot \
 dkms module-assistant patch git
 
-#Extracting tools
+# Extracting tools
 sudo apt-get install -y rar unrar zip unzip p7zip-full p7zip-rar arj zoo cabextract uudeview mpack unace sharutils
 
-#Java
+# Java
 sudo apt-get install -y openjdk-8-jdk
 
-#dev
+# dev
 sudo apt-get install -y npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node #by default executable is **nodejs**, but bower expects **node**, see http://stackoverflow.com/questions/20886217/browserify-error-usr-bin-env-node-no-such-file-or-directory
 sudo apt-get install -y mvn #maven
 sudo apt-get install -y zeal #offline documentations
 
-#Codecs
+# Codecs
 sudo apt-get -y install ubuntu-restricted-extras #TODO: asks for manual configuration
 sudo apt-get -y install id3tool lame libjpeg-progs  flac faac faad sox  ffmpeg2theora libmpeg3-1  mpegdemux x264
 
-#fonts
+# fonts
 sudo apt-get -y install fonts-roboto
 sudo apt-get -y install fonts-noto
 sudo apt-get -y install fonts-inconsolata
 sudo apt-get -y install ttf-anonymous-pro
 
-#System tools
+# System tools
 sudo apt-get -y install gparted synaptic
 sudo apt-get -y install unity-tweak-tool
 sudo apt -y install xdotool # using to set the default workspace
 
-#OTHERS
+# Other apps
 sudo apt-get -y install guake
 sudo apt-get -y install vim-gtk vim-syntax-gtk vim-addon-manager vim-common
 sudo apt-get -y install nautilus-dropbox
@@ -73,13 +73,13 @@ sudo apt-get -y install virtualbox virtualbox-guest-additions-iso
 sudo apt-get -y install putty
 sudo apt-get -y install htop #a beautiful top
 
-#uninstall
+# uninstall
 sudo apt-get -y purge transmission-* #Default torrent client
 sudo apt-get autoremove
 
-#Add external repos TODO: it asks to press enter do add the repository
+# Add external repos TODO: it asks to press enter do add the repository
 
-#spotify
+# Spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo add-apt-repository ppa:webupd8team/atom
@@ -87,21 +87,30 @@ sudo add-apt-repository ppa:webupd8team/atom
 
 sudo apt-get update
 
-#install packages from external repos
+# install packages from external repos
 sudo apt-get install -y spotify-client
 sudo apt-get install -y atom
 
-#Updating IDS Hardware List - helps to recognize unknown devices
+# TODO: add snaps: atom, telegram, htop
+
+# Updating IDS Hardware List - helps to recognize unknown devices
 sudo update-pciids && update-usbids
 
-#Making VLC the default video player
+# Making VLC the default video player
 sudo sed -i 's/totem/vlc/g' /usr/share/gnome/applications/defaults.list
 
-#Startup applications
+# Startup applications
 #error
 #sed -i 's/Autostart-enabled=false/Autostart-enabled=true/g' $HOME/.config/autostart/guake.desktop #guake
 
-#Restore VIM backup
+######################
+#   Restore bashrc   #
+######################
+cat bashrc >> $HOME/.bashrc
+
+######################
+# Restore VIM backup #
+######################
 VIM_DIR=$HOME/.vim
 VIM_COLOR_DIR=$VIM_DIR/colors
 VIM_COLOR_TAR_URL=https://github.com/AlessandroYorba/Sierra/archive/v2.2.2.tar.gz
